fd592a38d4a559c6adb0dc9733cd10fa2578f16a
Muhamed Hassan <muhamedhassan8@icloud.com>
2019-08-08 01:55:05 +0200

    cleaning up

--- rate_dactra/__init__.py
+++ rate_dactra/__init__.py
@@ -1,36 +1,26 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_uploads import configure_uploads, patch_request_class, UploadSet, IMAGES
-
-from config import config
+from .config import config
 from .models import db, login_manager
 from .navbars import nav
-
 photos: UploadSet = UploadSet('photos', IMAGES)
 bootstrap = Bootstrap()
-
-
 def create_app(configuration='default'):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config[configuration])
-
     bootstrap.init_app(app)
     db.init_app(app)
     nav.init_app(app)
     login_manager.init_app(app)
     configure_uploads(app, photos)
     patch_request_class(app)
-
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
-
     from .admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint)
-
     from .teacher import teacher as teacher_blueprint
     app.register_blueprint(teacher_blueprint)
-
     return app
--- rate_dactra/rate_dactra.py
+++ rate_dactra/rate_dactra.py
@@ -1,11 +1,11 @@
-import os
-
-from rate_dactra import create_app, db
-from rate_dactra.models import User, Teacher, Review, Comment
-
-app = create_app(configuration=os.getenv('FLASK_CONFIG') or 'default')
-
-
-@app.shell_context_processor
-def make_shell_context():
+import os                                                                                                                         
+                                                                                                                                  
+from rate_dactra import create_app, db                                                                                            
+from .models import User, Teacher, Review, Comment                                                                                
+                                                                                                                                  
+app = create_app(configuration=os.getenv('FLASK_CONFIG') or 'default')                                                            
+                                                                                                                                  
+                                                                                                                                  
+@app.shell_context_processor                                                                                                      
+def make_shell_context():                                                                                                         
     return dict(db=db, User=User, Teacher=Teacher, Review=Review, Comment=Comment)
