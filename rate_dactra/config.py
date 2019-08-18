4605f173045f859f740a5e4861495d50b34e6079
Muhamed Hassan <muhamedhassan8@icloud.com>
2019-08-18 14:50:55 +0200

    path error

--- rate_dactra/config.py
+++ rate_dactra/config.py
@@ -25,7 +25,7 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, '../db.sqlite3')
 
 
 config = {
