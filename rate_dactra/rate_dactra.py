1a0e5840206af4ef658ef9b878b864468c6f049e
Muhamed Hassan <muhamedhassan8@icloud.com>
2019-08-18 14:40:47 +0200

    Revert "removed flask-bootstrap"
    This reverts commit e99d0004

--- rate_dactra/rate_dactra.py
+++ rate_dactra/rate_dactra.py
@@ -3,7 +3,7 @@ import os
 from rate_dactra import create_app, db
 from .models import User, Teacher, Review, Comment
 
-app = create_app(configuration=os.getenv('FLASK_ENV') or 'default')
+app = create_app(configuration=os.getenv('FLASK_CONFIG') or 'default')                                                            
                                                                                                                                   
                                                                                                                                   
 @app.shell_context_processor                                                                                                      
